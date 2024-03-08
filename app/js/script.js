a614da384e54f67d177f4b4b807e1150619c50a9
LIBERT Brice <bricel67@gmail.com>
2024-03-08 17:29:59 +0100

    toggle color mode on click

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log('hello')
+const radioButtons = document.querySelectorAll('.toggle__wrapper input')
+console.log(radioButtons);
+
+for (let i = 0; i < radioButtons.length; i++) {
+  radioButtons[i].addEventListener('click', () => {
+    document.querySelector('#dark').checked
+      ? (document.querySelector('body').classList = 'dark')
+      : (document.querySelector('body').classList = 'light')
+  })
+
+}
