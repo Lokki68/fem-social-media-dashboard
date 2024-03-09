9685bdbb36eabf8090d7a98d406873c109e5b026
LIBERT Brice <bricel67@gmail.com>
2024-03-09 15:51:02 +0100

    color mode with local storage

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,44 @@
-const radioButtons = document.querySelectorAll('.toggle__wrapper input')
+const darkButton = document.querySelector('#dark')
+const lightButton = document.querySelector('#light')
 
-for (let i = 0; i < radioButtons.length; i++) {
-  radioButtons[i].addEventListener('click', () => {
-    document.querySelector('#dark').checked
-      ? (document.querySelector('body').classList = 'dark')
-      : (document.querySelector('body').classList = 'light')
-  })
-
+const setColorMode = () => {
+  if (localStorage.getItem('colorMode') === 'dark') {
+    setDarkMode()
+  } else {
+    setLightMode()
+  }
 }
+
+const setDarkMode = () => {
+  document.querySelector('body').classList = 'dark'
+  localStorage.setItem('colorMode', 'dark')
+}
+
+const setLightMode = () => {
+  document.querySelector('body').classList = 'light'
+  localStorage.setItem('colorMode', 'light')
+}
+
+const colorModeFormLocalStorage = () => {
+  return localStorage.getItem('colorMode')
+}
+
+const colorModeFromPreference = () => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+}
+
+const loadAndUpdateColor = () => {
+  const color = colorModeFormLocalStorage() || colorModeFromPreference()
+  color === 'dark' ? setDarkMode() : setLightMode()
+}
+
+const radioButtons = document.querySelectorAll('.toggle__wrapper input')
+radioButtons.forEach(radio => {
+  radio.addEventListener('click', () => darkButton.checked ? setDarkMode() : setLightMode())
+})
+
+
+window.matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', e => {
+    e.matches ? setDarkMode() : setLightMode()
+  })
